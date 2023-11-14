582181c5c006a2246a720a9250185a54431a3de9
jwblau2 <jeremy@abundantly.com>
2023-11-13 23:44:18 -0700

    added to typeDefs and index.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,16 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookInput: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
