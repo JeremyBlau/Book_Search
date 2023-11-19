6ee1659afd6a5d23a827ccc29396cff70e74b667
jwblau2 <jeremy@abundantly.com>
2023-11-19 16:01:44 -0700

    added to the resolvers.js page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,83 @@
-const { User } = require('..models/');
+const { User } = require('../models/');
+const bcrypt = require('bcrypt');
 
 const resolvers = {
   Query: {
-    me: (parent, args, context) => {
-      // Logic to fetch current user
+    me: async (parent, args, context) => {
+      if (!context.user) {
+        throw new Error('Authentication error. Please log in.');
+      }
+      return await User.findById(context.user._id);
     },
   },
   Mutation: {
     login: async (parent, { email, password }, context) => {
-      // Logic to handle user login
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new Error('Invalid credentials');
+      }
+
+      const validPassword = await bcrypt.compare(password, user.password);
+
+      if (!validPassword) {
+        throw new Error('Invalid credentials');
+      }
+
+      // Create JWT token or set session for authentication
+      // Example with JWT:
+      // const token = createToken(user);
+      // context.req.session.token = token;
+
+      return user;
     },
     addUser: async (parent, { username, email, password }, context) => {
-      // Logic to add a new user
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        throw new Error('User already exists');
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newUser = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      // Create JWT token or set session for authentication for the new user
+      // Example with JWT:
+      // const token = createToken(newUser);
+      // context.req.session.token = token;
+
+      return newUser;
     },
     saveBook: async (parent, { bookInput }, context) => {
-      // Logic to save a book for a user
+      if (!context.user) {
+        throw new Error('Authentication error. Please log in.');
+      }
+
+      const user = await User.findByIdAndUpdate(
+        context.user._id,
+        { $push: { savedBooks: bookInput } },
+        { new: true }
+      );
+
+      return user;
     },
     removeBook: async (parent, { bookId }, context) => {
-      // Logic to remove a book from a user's saved books
+      if (!context.user) {
+        throw new Error('Authentication error. Please log in.');
+      }
+
+      const user = await User.findByIdAndUpdate(
+        context.user._id,
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+
+      return user;
     },
   },
 };
