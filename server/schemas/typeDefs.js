92652acf03e244aa7d5ea7b04ae387845b50f789
jwblau2 <jeremy@abundantly.com>
2023-11-19 15:53:35 -0700

    added to the typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,42 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+    bookCount: Int
+  }
+
   type Query {
     me: User
   }
 
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
   type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
     saveBook(bookInput: BookInput!): User
-    removeBook(bookId: ID!): User
+    removeBook(bookId: String!): User
   }
 `;
 
