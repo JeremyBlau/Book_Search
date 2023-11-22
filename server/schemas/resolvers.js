b0b23fd223fcda20dd28a203b7a708057017246c
jwblau2 <jeremy@abundantly.com>
2023-11-21 18:41:14 -0700

    added to componeents

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,10 +5,14 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      if (context.user) {}
-      throw AuthenticationError
+        if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            return userData;
+        }
+        throw new AuthenticationError('Not logged in');
     }
-  },
+},
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
